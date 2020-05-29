ae81af2ffa5f7b9807f7851f979074cda0ca596b
Dmitry Kurbatov <dk@dimcha.ru>
2020-05-30 00:13:21 +0300

    cosmetics for parser code

--- parser.go
+++ parser.go
@@ -1,90 +1,56 @@
-package main
+package sss
 
 import (
 	"errors"
 	"fmt"
-	"log"
 )
 
-func BuildTrees(str *string) ([]*Tree, error) {
-	var trees []*Tree
+// Build all possible trees from input string
+func ParseStringToTrees(str *string) ([]Tree, error) {
+	var trees []Tree
 	scanner := NewScanner(str)
 
 	t := NewTree()
-	var tok = Token{}
-	for tok.Type != EOF && tok.Type != BAD {
-		tok = scanner.Read()
-		log.Printf("T: %s, V: %s", tokenMap[tok.Type], tok.Value)
+	var token = Token{}
+	for token.Type != EOF && token.Type != BAD {
+		token = scanner.Read()
 
-		switch tok.Type {
+		switch token.Type {
 		case BAD:
-			return nil, errors.New(fmt.Sprintf("bad input '%s' at position %d", tok.Value, scanner.pos))
+			return nil, errors.New(fmt.Sprintf("bad input '%s' at position %d", token.Value, scanner.pos))
 		case LITERAL:
-			t.AddChild(LITERAL, tok.Value)
-			log.Printf("added child")
+			t.AddChild(LITERAL, token.Value)
 		case LSBRACKET, LBRACKET, LBRACE:
-			t.AddChildAndMoveOnto(NodeType(tok.Type), tok.Value)
-			log.Printf("added child and move onto")
+			t.AddChildAndMoveOnto(NodeType(token.Type), token.Value)
 		case RSBRACKET:
-			log.Printf("move up")
-			typ, val := t.GetCurrentNodeValues()
+			typ, _ := t.GetCurrentNodeValues()
 			if typ != LSBRACKET {
-				log.Printf("abnormal string at pos: %d, rune: '%s' (type: %d)", scanner.pos, val, typ)
-				trees = append(trees, t)
+				trees = append(trees, *t)
 				t = NewTree()
 				continue
 			}
 			t.ChangeNode(SBRACKETS, "[]")
-			log.Printf("node changed")
-			_, err := t.MoveUp()
-			if err != nil {
-				log.Printf("broken tree at pos: %d, rune: '%s' (type: %d): can't move up: %s", scanner.pos, val, typ, err)
-				trees = append(trees, t)
-				t = NewTree()
-				continue
-			}
-			log.Printf("move up")
+			t.MoveUp()
 		case RBRACKET:
-			log.Printf("move up")
-			typ, val := t.GetCurrentNodeValues()
+			typ, _ := t.GetCurrentNodeValues()
 			if typ != LBRACKET {
-				log.Printf("abnormal string at pos: %d, rune: '%s' (type: %d)", scanner.pos, val, typ)
-				trees = append(trees, t)
+				trees = append(trees, *t)
 				t = NewTree()
 				continue
 			}
 			t.ChangeNode(BRACKETS, "()")
-			log.Printf("node changed")
-			_, err := t.MoveUp()
-			if err != nil {
-				log.Printf("broken tree at pos: %d, rune: '%s' (type: %d): can't move up: %s", scanner.pos, val, typ, err)
-				trees = append(trees, t)
-				t = NewTree()
-				continue
-			}
-			log.Printf("move up")
+			t.MoveUp()
 		case RBRACE:
-			log.Printf("move up")
-			typ, val := t.GetCurrentNodeValues()
+			typ, _ := t.GetCurrentNodeValues()
 			if typ != LBRACE {
-				log.Printf("abnormal string at pos: %d, rune: '%s' (type: %d)", scanner.pos, val, typ)
-				trees = append(trees, t)
+				trees = append(trees, *t)
 				t = NewTree()
 				continue
 			}
 			t.ChangeNode(BRACES, "{}")
-			log.Printf("node changed")
-			_, err := t.MoveUp()
-			if err != nil {
-				log.Printf("broken tree at pos: %d, rune: '%s' (type: %d): can't move up: %s", scanner.pos, val, typ, err)
-				trees = append(trees, t)
-				t = NewTree()
-				continue
-			}
-			log.Printf("move up")
+			t.MoveUp()
 		case EOF:
-			log.Printf("EOF reached")
-			trees = append(trees, t)
+			trees = append(trees, *t)
 			break
 		}
 	}
